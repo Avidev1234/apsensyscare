6410fc78fcc327da5eb268e30ca048661858d537
Krishna <kp432083@gmail.com>
2023-04-27 19:57:36 +0530

    skeleton added to cat page

--- aspensyscare/src/Components/LandingPage/Home/Home.jsx
+++ aspensyscare/src/Components/LandingPage/Home/Home.jsx
@@ -28,7 +28,7 @@ display:flex;
 const Home = () => {
   useEffect(() => {
     // 👇️ scroll to top on page load
-    // window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
   }, []);
   return (
     <Homecont>
