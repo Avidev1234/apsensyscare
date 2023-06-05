fb6ae0b2e2ec5184c9aeadd862fb661c9e591d29
Rudhiranjan2001 <silicon.rudhi563@gmail.com>
2023-06-05 21:11:23 +0530

    new design hover  page by rudhi

--- aspensyscare/src/Components/layouts/CategoryLayout/CategoryLayout.js
+++ aspensyscare/src/Components/layouts/CategoryLayout/CategoryLayout.js
@@ -15,7 +15,7 @@ const Drawer = ({ categoryid }) => {
     return true;
   })
   return (
-    <div className='w-[70%] flex flex-wrap  content-start	justify-between p-5 bg-white h-auto'>
+    <div className='w-[100%] lg:w-[1024px] xl:w-[1440px] flex flex-wrap  content-start	justify-between p-5 bg-white h-auto'>
       {!Products.loading && Products.products.product !== undefined && categoryProducts.length !== 0 ? (
         categoryProducts.slice(0, 6).map((val, i) => {
           return (
@@ -37,21 +37,23 @@ const CategoryLayout = () => {
   const [isShown, setIsShown] = useState({ categoryid: '', show: false });
 
   return (
-    <div className='w-full flex flex-col flex-nowrap justify-center items-center h-[auto] '>
-      <div className='w-full lg:w-[1440px] flex flex-row flex-nowrap justify-around  items-center bg-[#0112FE] h-[50px]'>
-        <h2 className='text-[8px] md:text-[14px] text-white uppercase font-bold cursor-pointer hover:text-[#E34343]' onClick={() => navigate('/')}>Home</h2>
-        {!Category.loading && Category.category.category !== undefined ? (
-          category.map((val, i) => {
-            return (
-              <div onMouseEnter={() => setIsShown({ categoryid: val.id, show: true })}
-                onMouseLeave={() => setIsShown({ categoryid: '', show: false })}>
-                <h2 className='text-[8px] md:text-[14px] text-white uppercase font-bold cursor-pointer hover:text-[#E34343]'
-                  onClick={() => navigate(`/category/${val.category_url}`, { state: { id: val.id, val: val } })}>{val.category_name}</h2>
-              </div>
+    <div className='w-full flex flex-col flex-nowrap justify-center items-center h-[auto] ' onMouseLeave={() => setIsShown({ categoryid: '', show: false })}>
+      <div className='w-full flex flex-row flex-nowrap justify-around  items-center bg-[#0112FE] h-[50px]'>
+        <div className='w-full lg:w-[1024px] xl:w-[1440px] flex flex-row flex-nowrap justify-around  items-center bg-[#0112FE] h-[50px]'>
+          <h2 className='text-[8px] md:text-[14px] text-white uppercase font-bold cursor-pointer hover:text-[#E34343]' onClick={() => navigate('/')}>Home</h2>
+          {!Category.loading && Category.category.category !== undefined ? (
+            category.map((val, i) => {
+              return (
+                <div onMouseEnter={() => setIsShown({ categoryid: val.id, show: true })}
+                  >
+                  <h2 className='text-[8px] md:text-[14px] text-white uppercase font-bold cursor-pointer hover:text-[#E34343]'
+                    onClick={() => navigate(`/category/${val.category_url}`, { state: { id: val.id, val: val } })}>{val.category_name}</h2>
+                </div>
+              )
+            }
             )
-          }
-          )
-        ) : null}
+          ) : null}
+        </div>
       </div>
       {isShown.show && <Drawer categoryid={isShown.categoryid} />}
 
